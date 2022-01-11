4bbe3493da3db206261f44e452d6222a4ebe824a
D51947403 <singraul20codeblog@gmail.com>
2022-01-11 17:27:13 +0530

    Spring Boot Hello application

---
+++ helloapp/src/test/java/com/boot/hello/DemoApplicationTests.java
@@ -0,0 +1,14 @@
+package com.boot.hello;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	
+	@Test
+	void contextLoads() {
+	}
+
+}
