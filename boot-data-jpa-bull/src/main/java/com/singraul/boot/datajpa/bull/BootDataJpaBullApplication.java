141463c3cf13b09cbb382aef8826683d97650d94
D51947403 <singraul20codeblog@gmail.com>
2022-01-12 20:15:26 +0530

    Bull Application example

---
+++ boot-data-jpa-bull/src/main/java/com/singraul/boot/datajpa/bull/BootDataJpaBullApplication.java
@@ -0,0 +1,13 @@
+package com.singraul.boot.datajpa.bull;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootDataJpaBullApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootDataJpaBullApplication.class, args);
+	}
+
+}
