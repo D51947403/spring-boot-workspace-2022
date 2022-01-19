acd8c212e23bbf18aea140dafecb193d65f51391
D51947403 <singraul20codeblog@gmail.com>
2022-01-19 21:25:50 +0530

    GET restapi testing example using Junit test case and rest template

--- boot-restapi-product/src/test/java/com/singraul/boot/restapi/product/BootRestapiProductApplicationTests.java
+++ boot-restapi-product/src/test/java/com/singraul/boot/restapi/product/BootRestapiProductApplicationTests.java
@@ -2,12 +2,22 @@ package com.singraul.boot.restapi.product;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.util.Assert;
+import org.springframework.web.client.RestTemplate;
+
+import com.singraul.boot.restapi.product.entity.Product;
 
 @SpringBootTest
 class BootRestapiProductApplicationTests {
-
+	@SuppressWarnings("deprecation")
 	@Test
-	void contextLoads() {
+	void testGetApi() {
+	RestTemplate  rest = new RestTemplate();
+	Product product = rest.getForObject("http://localhost:8080/product-rest-api/products/102", Product.class);
+	System.out.println("product "+product);
+	   Assert.notNull(product);
+	   Assert.hasText(product.getProdName(), "Torch");
 	}
+	
 
 }
