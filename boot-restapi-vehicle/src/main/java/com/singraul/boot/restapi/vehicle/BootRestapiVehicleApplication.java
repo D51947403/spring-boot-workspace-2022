6b049af12f610a90bdb454fbddb188679b49daa0
D51947403 <singraul20codeblog@gmail.com>
2022-01-19 18:55:55 +0530

    Rest API Vehicle example

---
+++ boot-restapi-vehicle/src/main/java/com/singraul/boot/restapi/vehicle/BootRestapiVehicleApplication.java
@@ -0,0 +1,13 @@
+package com.singraul.boot.restapi.vehicle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootRestapiVehicleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootRestapiVehicleApplication.class, args);
+	}
+
+}
---
+++ boot-restapi-vehicle/src/test/java/com/singraul/boot/restapi/vehicle/BootRestapiVehicleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.singraul.boot.restapi.vehicle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootRestapiVehicleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
