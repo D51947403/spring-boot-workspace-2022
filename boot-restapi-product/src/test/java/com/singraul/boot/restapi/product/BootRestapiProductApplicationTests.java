b511201c93e856b496d422f5ea169d6767835f43
D51947403 <singraul20codeblog@gmail.com>
2022-01-20 14:49:16 +0530

    POST API Testing using REST Template and JUnit

--- boot-restapi-product/src/test/java/com/singraul/boot/restapi/product/BootRestapiProductApplicationTests.java
+++ boot-restapi-product/src/test/java/com/singraul/boot/restapi/product/BootRestapiProductApplicationTests.java
@@ -9,15 +9,29 @@ import com.singraul.boot.restapi.product.entity.Product;
 
 @SpringBootTest
 class BootRestapiProductApplicationTests {
+	RestTemplate  rest = new RestTemplate();
 	@SuppressWarnings("deprecation")
 	@Test
 	void testGetApi() {
-	RestTemplate  rest = new RestTemplate();
+	
 	Product product = rest.getForObject("http://localhost:8080/product-rest-api/products/102", Product.class);
 	System.out.println("product "+product);
 	   Assert.notNull(product);
 	   Assert.hasText(product.getProdName(), "Torch");
 	}
-	
+	@Test
+  void testCreateProduct() {
+	  
+	 Product  productObj = new Product();
+	 productObj.setProdName("Samasung Galaxy");
+	 productObj.setProdDesc("This is best mobile in 4GB RAM memory.");
+	 productObj.setPrice(12000);
 
+	Product postForObject = rest.postForObject("http://localhost:8080/product-rest-api/products/", productObj, Product.class);
+	
+	Assert.notNull(postForObject);
+	Assert.notNull(postForObject.getProdId());
+	Assert.hasText("Samasung Galaxy", postForObject.getProdName());
+  }
+	
 }
