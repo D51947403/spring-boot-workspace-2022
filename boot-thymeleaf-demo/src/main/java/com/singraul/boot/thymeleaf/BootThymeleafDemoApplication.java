536a18af6a44a99aaf759bfa4ee992fa03b6c551
D51947403 <singraul20codeblog@gmail.com>
2022-01-24 21:50:48 +0530

    Spring Boot Thymeleaf demo

---
+++ boot-thymeleaf-demo/src/main/java/com/singraul/boot/thymeleaf/BootThymeleafDemoApplication.java
@@ -0,0 +1,14 @@
+package com.singraul.boot.thymeleaf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootThymeleafDemoApplication {
+
+	public static void main(String[] args) {
+		
+		SpringApplication.run(BootThymeleafDemoApplication.class, args);
+	}
+
+}
