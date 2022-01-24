5b20156b450463fd51b68c464d1fa0a67cd0d2c5
D51947403 <singraul20codeblog@gmail.com>
2022-01-24 22:13:25 +0530

    Thymeleaf expression language using ModelAndView

--- boot-thymeleaf-demo/src/main/java/com/singraul/boot/thymeleaf/controller/HelloController.java
+++ boot-thymeleaf-demo/src/main/java/com/singraul/boot/thymeleaf/controller/HelloController.java
@@ -3,12 +3,22 @@ package com.singraul.boot.thymeleaf.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class HelloController {
 
-	@RequestMapping(value="/hello" , method=RequestMethod.GET)
+	@RequestMapping(value = "/hello", method = RequestMethod.GET)
 	public String hello() {
 		return "hello";
+	}
+
+	@RequestMapping(value = "/sendData", method = RequestMethod.GET)
+	public ModelAndView sendData() {
+
+		ModelAndView mav = new ModelAndView("data");
+		mav.addObject("message", "Take up one idea and make your life easy.");
+		return mav;
+
 	}
 }
