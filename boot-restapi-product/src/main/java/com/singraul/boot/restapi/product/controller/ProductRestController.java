4bb638bf1d9ef68cfa74f1878327314006ad5bad
D51947403 <singraul20codeblog@gmail.com>
2022-01-24 13:39:27 +0530

    Use of slf4j logger in spring boot

--- boot-restapi-product/src/main/java/com/singraul/boot/restapi/product/controller/ProductRestController.java
+++ boot-restapi-product/src/main/java/com/singraul/boot/restapi/product/controller/ProductRestController.java
@@ -2,6 +2,8 @@ package com.singraul.boot.restapi.product.controller;
 
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -16,24 +18,31 @@ import com.singraul.boot.restapi.product.repos.ProductRepository;
 public class ProductRestController {
 	@Autowired
 	ProductRepository prodRepository;
+	
+	private static final Logger log=LoggerFactory.getLogger(ProductRestController.class);
 
 	@RequestMapping(value = "/products", method = RequestMethod.GET)
 	public List<Product> getAllProduct() {
+		log.info("Method : getAllProduct ");
 		return prodRepository.findAll();
 	}
 
 	@RequestMapping(value = "/products/{prodId}", method = RequestMethod.GET)
 	public Product getProduct(@PathVariable("prodId") int prodId) {
+		log.info("Method : getProduct ");
+		log.info("Parameter : prodId "+prodId);
 		return prodRepository.findById(prodId).get();
 	}
 
 	@RequestMapping(value = "/products", method = RequestMethod.POST)
 	public Product createProduct(@RequestBody Product product) {
+		log.info("Method : createProduct ");
 		return prodRepository.save(product);
 	}
 
 	@RequestMapping(value = "/products", method = RequestMethod.PUT)
 	public Product updateProduct(@RequestBody Product product) {
+		log.info("Method : updateProduct ");
 		// save method first check object in table
 		// if present then it update
 		// and if not it will create
@@ -42,6 +51,8 @@ public class ProductRestController {
 
 	@RequestMapping(value = "/products/{prodId}", method = RequestMethod.DELETE)
 	public void deleteProduct(@PathVariable("prodId") int prodId) {
+		log.info("Method : getProduct ");
+		log.info("Parameter : prodId "+prodId);
 		prodRepository.deleteById(prodId);
 	}
 
