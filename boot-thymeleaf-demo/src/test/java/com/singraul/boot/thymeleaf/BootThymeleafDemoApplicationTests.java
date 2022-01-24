d45663567d9e1bbe0282daf72e6dbf9b8f54af97
D51947403 <singraul20codeblog@gmail.com>
2022-01-24 21:51:19 +0530

    Spring Boot Thymeleaf example

---
+++ boot-thymeleaf-demo/src/test/java/com/singraul/boot/thymeleaf/BootThymeleafDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.singraul.boot.thymeleaf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootThymeleafDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
