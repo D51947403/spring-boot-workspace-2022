8eb41bc139a7d3ed0769b6a7f3eab7c01bf8b6fc
D51947403 <singraul20codeblog@gmail.com>
2022-01-25 16:40:38 +0530

    Basic Custom authentication in Spring Boot. Implementation of WebSecurityConfigurerAdapter

---
+++ boot-security-demo/src/main/java/com/singraul/boot/security/controller/CityController.java
@@ -0,0 +1,15 @@
+package com.singraul.boot.security.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CityController {
+   
+	@RequestMapping(value="/city", method=RequestMethod.GET)
+	public String getCity() {
+		return "Welcome to Satna city";
+	}
+	
+}
---
+++ boot-security-demo/src/main/java/com/singraul/boot/security/model/City.java
@@ -0,0 +1,24 @@
+package com.singraul.boot.security.model;
+
+public class City {
+	
+	private String cityName;
+	private long pincode;
+
+	public String getCityName() {
+		return cityName;
+	}
+
+	public void setCityName(String cityName) {
+		this.cityName = cityName;
+	}
+
+	public long getPincode() {
+		return pincode;
+	}
+
+	public void setPincode(long pincode) {
+		this.pincode = pincode;
+	}
+
+}
