920fe13c419f2070fb4f608e68d489314f083165
D51947403 <singraul20codeblog@gmail.com>
2022-01-25 21:16:40 +0530

    Coupon microservice example in spring boot

---
+++ boot-coupon-service/src/main/java/com/singraul/coupon/controller/CouponController.java
@@ -0,0 +1,29 @@
+package com.singraul.coupon.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.singraul.coupon.model.Coupon;
+import com.singraul.coupon.repos.CouponRepo;
+
+@RestController
+@RequestMapping("/coupon-rest-api")
+public class CouponController {
+
+	@Autowired
+	CouponRepo couponRepo;
+
+	@RequestMapping(value = "/coupon", method = RequestMethod.POST)
+	public Coupon createCoupon(@RequestBody Coupon coupon) {
+		return couponRepo.save(coupon);
+	}
+
+	@RequestMapping(value = "/coupon/{code}", method = RequestMethod.GET)
+	public Coupon getCouponByCode(@PathVariable("code") String code) {
+		return couponRepo.getCouponByCode(code);
+	}
+}
---
+++ boot-coupon-service/src/main/java/com/singraul/coupon/repos/CouponRepo.java
@@ -0,0 +1,11 @@
+package com.singraul.coupon.repos;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.singraul.coupon.model.Coupon;
+
+public interface CouponRepo extends JpaRepository<Coupon, Long> {
+
+	Coupon getCouponByCode(String code);
+
+}
