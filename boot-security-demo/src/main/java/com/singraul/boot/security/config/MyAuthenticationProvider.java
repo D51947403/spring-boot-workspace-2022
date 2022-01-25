156fd4466391a357893954dad4257eb7cbb736bb
D51947403 <singraul20codeblog@gmail.com>
2022-01-25 17:50:37 +0530

    Custom Authentication Provider example in spring Boot

---
+++ boot-security-demo/src/main/java/com/singraul/boot/security/config/MyAuthenticationProvider.java
@@ -0,0 +1,31 @@
+package com.singraul.boot.security.config;
+
+import java.util.Arrays;
+
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.stereotype.Component;
+
+@Component
+public class MyAuthenticationProvider implements AuthenticationProvider {
+
+	@Override
+	public Authentication authenticate(Authentication authentication) throws AuthenticationException {
+		String name = authentication.getName();
+		String password = authentication.getCredentials().toString();
+	    if("devendra".equals(name) && "kumar".equals(password)) {
+	    	return new UsernamePasswordAuthenticationToken(name, password, Arrays.asList());
+	    }else {
+	    	throw new BadCredentialsException("Invalid user name or password");
+	    }
+	}
+
+	@Override
+	public boolean supports(Class<?> authentication) {
+		return authentication.equals(UsernamePasswordAuthenticationToken.class);
+	}
+
+}
--- boot-security-demo/src/main/java/com/singraul/boot/security/config/MySecurityConfig.java
+++ boot-security-demo/src/main/java/com/singraul/boot/security/config/MySecurityConfig.java
@@ -1,34 +1,44 @@
 package com.singraul.boot.security.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
 public class MySecurityConfig extends WebSecurityConfigurerAdapter {
 
+	@Autowired
+	private MyAuthenticationProvider  authenticationProvider;
+	
+	/*
+	 * @Override protected void configure(AuthenticationManagerBuilder auth) throws
+	 * Exception {
+	 * 
+	 * BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+	 * InMemoryUserDetailsManager userServiceDetail = new
+	 * InMemoryUserDetailsManager(); UserDetails userDetail =
+	 * User.withUsername("devendra").password(passwordEncoder.encode("kumar"))
+	 * .authorities("read").build(); userServiceDetail.createUser(userDetail);
+	 * 
+	 * auth.userDetailsService(userServiceDetail).passwordEncoder(passwordEncoder);
+	 * 
+	 * }
+	 */
+
+	
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-
-		BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
-		InMemoryUserDetailsManager userServiceDetail = new InMemoryUserDetailsManager();
-		UserDetails userDetail = User.withUsername("devendra").password(passwordEncoder.encode("devendra"))
-				.authorities("read").build();
-		userServiceDetail.createUser(userDetail);
-
-		auth.userDetailsService(userServiceDetail).passwordEncoder(passwordEncoder);
+      auth.authenticationProvider(authenticationProvider);
 
 	}
-
+	
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http.httpBasic();
 		http.authorizeRequests().anyRequest().authenticated();
 
 	}
+
 }
