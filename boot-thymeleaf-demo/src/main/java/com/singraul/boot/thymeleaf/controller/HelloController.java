dd44c242569035305e7208780651c5b4cebd8823
D51947403 <singraul20codeblog@gmail.com>
2022-01-25 11:15:16 +0530

    Rendering Student List using Thymeleaf

--- boot-thymeleaf-demo/src/main/java/com/singraul/boot/thymeleaf/controller/HelloController.java
+++ boot-thymeleaf-demo/src/main/java/com/singraul/boot/thymeleaf/controller/HelloController.java
@@ -1,5 +1,8 @@
 package com.singraul.boot.thymeleaf.controller;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -23,13 +26,41 @@ public class HelloController {
 		return mav;
 
 	}
+
 	@RequestMapping(value = "/student", method = RequestMethod.GET)
 	public ModelAndView getStudent() {
 		ModelAndView mav = new ModelAndView("student");
-		Student student= new Student();
+		Student student = new Student();
 		student.setName("Devendra");
 		student.setScore(86);
 		mav.addObject("studentObj", student);
+		return mav;
+	}
+	@RequestMapping(value = "/studentList", method = RequestMethod.GET)
+	public ModelAndView getStudentList() {
+
+		ModelAndView mav = new ModelAndView("studentList");
+
+		Student student = new Student();
+		student.setName("Devendra");
+		student.setScore(86);
+
+		Student student2 = new Student();
+		student2.setName("Rahul");
+		student2.setScore(96);
+
+		Student student3 = new Student();
+		student3.setName("Ragni");
+		student3.setScore(76);
+
+		Student student4 = new Student();
+		student4.setName("Ranjana");
+		student4.setScore(80);
+
+		List<Student> studentList = Arrays.asList(student, student2, student3, student4);
+		
+		mav.addObject("studentList", studentList);
+
 		return mav;
 	}
 }
