5d93c3279b0a85cbddf3fc16949b1cf5d849a1ea
D51947403 <singraul20codeblog@gmail.com>
2022-01-25 21:17:04 +0530

    coupon rest-api

---
+++ boot-coupon-service/src/main/java/com/singraul/coupon/BootCouponServiceApplication.java
@@ -0,0 +1,13 @@
+package com.singraul.coupon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootCouponServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootCouponServiceApplication.class, args);
+	}
+
+}
---
+++ boot-coupon-service/src/test/java/com/singraul/coupon/BootCouponServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.singraul.coupon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootCouponServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
