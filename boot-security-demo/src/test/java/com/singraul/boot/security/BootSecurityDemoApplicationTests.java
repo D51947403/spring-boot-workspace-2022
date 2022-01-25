81eb047bc8c5f574710ca124187487ce871a5e1c
D51947403 <singraul20codeblog@gmail.com>
2022-01-25 19:01:29 +0530

    Spring Boot Password encoder classes

--- boot-security-demo/src/test/java/com/singraul/boot/security/BootSecurityDemoApplicationTests.java
+++ boot-security-demo/src/test/java/com/singraul/boot/security/BootSecurityDemoApplicationTests.java
@@ -2,12 +2,19 @@ package com.singraul.boot.security;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.Pbkdf2PasswordEncoder;
+import org.springframework.security.crypto.scrypt.SCryptPasswordEncoder;
 
 @SpringBootTest
 class BootSecurityDemoApplicationTests {
 
 	@Test
-	void contextLoads() {
+	void testPasswordEncoder() {
+		System.out.println(new BCryptPasswordEncoder().encode("kumar"));
+		System.out.println(new Pbkdf2PasswordEncoder().encode("kumar"));
+		// this encoder requires BouncyCastle dependency 
+		System.out.println(new SCryptPasswordEncoder().encode("kumar"));
 	}
 
 }
