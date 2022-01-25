f0c62cab6d03f8c833aacec76a3aa23e06864359
D51947403 <singraul20codeblog@gmail.com>
2022-01-25 19:10:52 +0530

    DelegatingPasswordEncoder in spring boot

--- boot-security-demo/src/test/java/com/singraul/boot/security/BootSecurityDemoApplicationTests.java
+++ boot-security-demo/src/test/java/com/singraul/boot/security/BootSecurityDemoApplicationTests.java
@@ -1,8 +1,13 @@
 package com.singraul.boot.security;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.DelegatingPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.crypto.password.Pbkdf2PasswordEncoder;
 import org.springframework.security.crypto.scrypt.SCryptPasswordEncoder;
 
@@ -15,6 +20,14 @@ class BootSecurityDemoApplicationTests {
 		System.out.println(new Pbkdf2PasswordEncoder().encode("kumar"));
 		// this encoder requires BouncyCastle dependency 
 		System.out.println(new SCryptPasswordEncoder().encode("kumar"));
+		
+		Map<String,PasswordEncoder>  encoderMap=new HashMap<>();
+		
+		encoderMap.put("bcrypt", new BCryptPasswordEncoder());
+		encoderMap.put("scrypt", new SCryptPasswordEncoder());
+		
+		System.out.println(new DelegatingPasswordEncoder("bcrypt",encoderMap ).encode("kumar"));
+		
 	}
 
 }
