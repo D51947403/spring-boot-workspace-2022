5c329de898028f06a6e4596dd2c969d036db61b9
D51947403 <singraul20codeblog@gmail.com>
2022-01-27 22:07:06 +0530

    Spring Boot REST API Versioning example

---
+++ boot-restapi-versioning/src/main/java/com/singraul/versioning/BootRestapiVersioningApplication.java
@@ -0,0 +1,13 @@
+package com.singraul.versioning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootRestapiVersioningApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootRestapiVersioningApplication.class, args);
+	}
+
+}
---
+++ boot-restapi-versioning/src/test/java/com/singraul/versioning/BootRestapiVersioningApplicationTests.java
@@ -0,0 +1,13 @@
+package com.singraul.versioning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootRestapiVersioningApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
