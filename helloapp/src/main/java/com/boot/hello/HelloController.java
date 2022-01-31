876cca93190143bd23c18f41d4a27fdc6abe07ec
D51947403 <singraul20codeblog@gmail.com>
2022-01-31 10:14:51 +0530

    Reading list from property file using spring expression language

--- helloapp/src/main/java/com/boot/hello/HelloController.java
+++ helloapp/src/main/java/com/boot/hello/HelloController.java
@@ -1,13 +1,27 @@
 package com.boot.hello;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
+	// Use of Spring Expression Language 
+	@Value(value = "#{'${spring.project.names}'.split(',')}")
+	private List<String> springProjectList ;
+	
 	
 	@GetMapping("/hello")
 	public String hello() {
 		return "Hello Spring Boot Application";
+	}
+	
+	
+	@GetMapping("/getSpringProjectNames")
+	public List<String> getSpringProjectNames() {
+		System.out.println(springProjectList.toString());
+		return springProjectList;
 	}
 }
