36c511fea37fe5b52fd7302051b018f0600b9098
D51947403 <singraul20codeblog@gmail.com>
2022-02-15 16:28:32 +0530

    Spring Batch demo application

---
+++ boot-spring-batch-app/src/main/java/com/singraul/batch/Reader.java
@@ -0,0 +1,23 @@
+package com.singraul.batch;
+
+import org.springframework.batch.item.ItemReader;
+import org.springframework.batch.item.NonTransientResourceException;
+import org.springframework.batch.item.ParseException;
+import org.springframework.batch.item.UnexpectedInputException;
+
+public class Reader implements ItemReader<String> {
+
+	private String[] arr = { "Java Web Service", "MicroService", "RDBMS" };
+	private int count;
+
+	@Override
+	public String read() throws Exception, UnexpectedInputException, ParseException, NonTransientResourceException {
+		if(count < arr.length) {
+			return arr[count++];
+		}else {
+			count=0;
+		}
+		return null;
+	}
+
+}
