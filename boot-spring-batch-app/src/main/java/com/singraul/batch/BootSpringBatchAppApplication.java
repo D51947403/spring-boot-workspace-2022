3c79ed74cbaf71eeafe3ae4b8f9c2e15ad5002ef
D51947403 <singraul20codeblog@gmail.com>
2022-02-15 16:29:11 +0530

    Spring Batch application example

---
+++ boot-spring-batch-app/src/main/java/com/singraul/batch/BootSpringBatchAppApplication.java
@@ -0,0 +1,13 @@
+package com.singraul.batch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootSpringBatchAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootSpringBatchAppApplication.class, args);
+	}
+
+}
