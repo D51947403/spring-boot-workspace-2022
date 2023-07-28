66573d6b37a029c52a867773de497282b5869718
D51947403 <singraul20codeblog@gmail.com>
2023-07-28 19:18:19 +0530

    Basic Security in Spring boot

---
+++ boot-basic-security-app/src/main/java/com/singraul/app/BootBasicSecurityAppApplication.java
@@ -0,0 +1,15 @@
+package com.singraul.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+//https://cognizant.udemy.com/course/spring-security-fundamentals/learn/lecture/22681867#overview
+
+@SpringBootApplication
+public class BootBasicSecurityAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootBasicSecurityAppApplication.class, args);
+	}
+
+}
